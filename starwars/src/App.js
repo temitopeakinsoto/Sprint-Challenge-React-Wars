e03d2cef3c7e4ae978b757bddaf0da168923b982
Adetunji Awobajo <surfsamson@gmail.com>
2019-09-13 02:53:31 -0700

    feat: Add useState and useEffect hooks
    In this commit, I added the useEffect and useState hooks to the App.js. state properties
    were also set/initialized. the state props apiReturnInfo and apiCallError together with
    their respective setter functions were declared/initialized

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,21 +1,19 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 import Starwars from './components/Starwars';
 import './App.css'; 
 
 const App = () => {
 
-  axios.get('https://swapi.co/api/people')
-  .then((response) => {
-    console.log('this response', response.data)
-  })
-  .catch()
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
   // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+
+  const [apiCallError, setApiCallError] = useState(null);
+  const [apiReturnInfo, setApiReturnInfo] = useState({ bioData: [], film: [], vehicle: [], species: [], starship:[] });
 
   return (
     <div className="App">   
