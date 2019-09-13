6736e5314ba440c41566aaf20fca3afa371adcf8
Adetunji Awobajo <surfsamson@gmail.com>
2019-09-13 02:43:21 -0700

    feat: Create a sample styled component
    In this commit I created a sample styled component titled FilmComponentDiv. This component
    is used to apply styling to the div component for wrapping the Film component.
    
    The styled-component library was installed first, using npm, in order for this to be possible

--- starwars/src/components/Starwars.js
+++ starwars/src/components/Starwars.js
@@ -4,18 +4,34 @@ import Species from "./Species"
 import Vehicle from "./Vehicle";
 import Starship from "./Starship";
 import Biodata from "./Biodata";
+import styles from "styled-components";
+
+const FilmComponentDiv = styles.div`
+    height: 100px;
+    width: 100px;
+    background-color: red;
+`
 
 
 function Starwars(props) {
-    const { details } = props;
+    const { films, species, vehicles, starships } = props;
     return (
         <div>
-            <Biodata />
-            <Film />
-            <Species />  
-            <Vehicle />
-            <Starship />          
-
+            <div>
+                <Biodata />
+            </div>
+            <FilmComponentDiv>
+                <Film />
+            </FilmComponentDiv>
+            <div>
+                <Species />
+            </div>
+            <div>
+                <Vehicle />
+            </div>  
+            <div>
+                <Starship /> 
+            </div>
         </div>
         
     )
